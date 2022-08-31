25c870b9bb870978db00634b552a3b915cd6b4d5
lakotelman <la.n.kot@pm.me>
2022-08-30 16:39:31 -0800

    configuring daisyui

--- frontend/src/views/Visions.tsx
+++ frontend/src/views/Visions.tsx
@@ -1,11 +1,14 @@
 import { DefaultLayout } from "../layouts/Default"
 import TabContainer, {Colors} from "../components/TabContainer";
-
+import VisionCard from "../components/Visions/VisionCard";
 
 function Visions(){ 
     return (
         <TabContainer color={Colors.lightGreen}>
-            <p>Coming Soon!</p>
+            <div>
+                <p>Coming Soon!</p>
+                <VisionCard/>
+            </div>
         </TabContainer>
       );
 }
